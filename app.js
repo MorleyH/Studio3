7e9c11d136af7b3ac302f0c56608e29abd300911
MorleyH <morley_hawriluk@brown.edu>
2022-09-28 20:57:01 -0400

    first

--- app.js
+++ app.js
@@ -26,16 +26,26 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  const filteredArray = fruitsArray.filter((item) => {
+     
+      return item.nutritions.sugar>15;
+  })
+  const keyArray = filteredArray.map((item) => {
+    return item.name;
+  })
 
   // TODO: Create a new HTML element to display your data
-
+  const newElement = document.createElement('p');
   // TODO: Append your new element to the page
-
+  newElement.innerHTML = "" + keyArray;
+  const existingElement = document.getElementById('cs1300-gallery');
+  existingElement.append(newElement);
 }
+
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
 const exampleAddElement = () => {
